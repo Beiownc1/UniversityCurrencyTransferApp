e17265f6fdf4dd463ec32fa758878c95a9d17c6b
Beiownc1 <najafuni@hotmail.com>
2025-12-29 00:34:38 +0000

    rename database folder

--- config/dbConnect.php
+++ config/dbConnect.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO('sqlite:' . dirname(__DIR__) . '/Zdatabase/database.db');
+$pdo = new PDO('sqlite:' . dirname(__DIR__) . '/Database/database.db');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->exec('PRAGMA foreign_keys = ON'); // enforce foreign keys
 ?>
